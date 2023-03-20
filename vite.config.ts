4786cd0a60f5a136bfa6de943d145dc4901cd126
S <s@Mac-mini.local>
2023-03-20 21:06:05 +0900

    url周り調整

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/seamless-color-ball/',
+  base: './',
   build: {
     outDir: 'docs',
   },
